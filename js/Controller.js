b225e274d8792cdd99ed41c9de20b5e3f39ed758
hhyanghh <yanghh711@naver.com>
2023-02-25 21:04:24 +0900

    검색결과 구현 : 검색결과가 있을경우, 없을경우

--- js/Controller.js
+++ js/Controller.js
@@ -1,11 +1,12 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView }) {
+  constructor(store, { searchFormView, searchResultView }) {
     console.log(tag);
     this.store = store;
 
     this.searchFormView = searchFormView;
+    this.searchResultView = searchResultView;
 
     this.subscribeViewEvents();
   }
@@ -15,11 +16,20 @@ export default class Controller {
       .on("@reset", () => this.reset());
   }
 
-  search(keyword) {
-    console.log(tag, keyword);
+  search(searchKeyword) {
+    this.store.search(searchKeyword);
+    this.render();
   }
 
   reset() {
     console.log(tag, "reset");
+  }
+
+  render() {
+    if (this.store.searchKeyword.length > 0) {
+      this.searchResultView.show(this.store.searchResult);
+      return;
+    }
+    this.searchResultView.hide();
   }
 }
