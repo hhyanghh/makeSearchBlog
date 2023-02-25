c38cdf28bda7f33f225263b98bc06d1e3113804a
hhyanghh <yanghh711@naver.com>
2023-02-25 11:14:00 +0900

    초기 검색폼 설정

--- js/Controller.js
+++ js/Controller.js
@@ -1,38 +1,10 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView }) {
+  constructor(store, { searchFormView }) {
     console.log(tag);
     this.store = store;
 
-    // TODO
     this.searchFormView = searchFormView;
-    this.searchResultView = searchResultView;
-
-    // view의 event를 수신
-    this.subscribeViewEvnets();
-  }
-  subscribeViewEvnets() {
-    this.searchFormView
-      .on("@submit", (event) => this.search(event.detail.value))
-      .on("@reset", () => this.reset());
-  }
-
-  search(searchKeyword) {
-    console.log(tag, "search", searchKeyword);
-    this.store.search(searchKeyword);
-    this.render();
-  }
-
-  reset() {
-    console.log(tag, "reset method를 받는다.");
-  }
-
-  render() {
-    if (this.store.searchKeyword.length > 0) {
-      this.searchResultView.show(this.store.searchResult);
-      return;
-    }
-    this.searchResultView.hide();
   }
 }
--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ import Controller from "./Controller.js";
 import Store from "./store.js";
 import storage from "./storage.js";
 import SearchFormView from "./views/SearchFormView.js";
-import SearchResultView from "./views/SearchResultView.js";
 
 const tag = "[main]";
 document.addEventListener("DOMContentLoaded", main);
@@ -14,7 +13,6 @@ function main() {
   const views = {
     // TODO
     searchFormView: new SearchFormView(),
-    SearchResultView: new SearchResultView(),
   };
 
   new Controller(store, views);
